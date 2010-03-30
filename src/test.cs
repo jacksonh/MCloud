cb623f5f8d8666f31e8b595c2a0e74ddeee2e96f
Jackson Harper <jackson@novell.com>
2010-03-30 16:28:28 -0400

    user supplied key

--- src/test.cs
+++ src/test.cs
@@ -10,13 +10,17 @@ using MCloud.Linode;
 
 public class T {
 
-	public static void Main ()
+	public static void Main (string [] args)
 	{
-		LinodeDriver driver = new LinodeDriver ("iulALXI8yPVLudSIWTKvRfdEqJZmtkKYayMucS6xr5lWugGofTS5QGpiwTBI8opO", null);
+		if (args.Length < 1) {
+			Console.WriteLine ("You need to supply your Linode API key.");
+			return;
+		}
+		
+		LinodeDriver driver = new LinodeDriver (args [0], null);
 
 		List<Node> nodes = driver.ListNodes ();
 
-		Console.WriteLine ("got nodes: {0}", nodes);
 		foreach (Node node in nodes) {
 			Console.WriteLine ("node: {0}", node);
 			node.Reboot ();
