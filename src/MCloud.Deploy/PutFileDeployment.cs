ae6149c7cc8c497073300c5d6aa73a38449db221
Jackson Harper <jackson@novell.com>
2010-06-17 09:35:46 -0400

    Docs, arg checking

--- src/MCloud.Deploy/PutFileDeployment.cs
+++ src/MCloud.Deploy/PutFileDeployment.cs
@@ -6,20 +6,36 @@ using Tamir.SharpSsh;
 
 namespace MCloud.Deploy {
 
+	/// <summary>
+	/// Put a single file from the local machine onto the remote machine.
+	/// If no remote directory is supplied the file will be put in the /root/
+	/// directory.
+	/// </summary>
 	public class PutFileDeployment : PutFilesDeployment {
 
-		public PutFileDeployment (string local) : base ()
+		/// <summary>
+		/// put the specified file on the server in the /root/ directory
+		/// </summary>
+		public PutFileDeployment (string local) : this (local, DefaultRemoteDirectory)
 		{
-			Files.Add (local);
 		}
 
+		/// <summary>
+		/// Put the specified file in the specified directory.
+		/// </summary>
 		public PutFileDeployment (string local, string remote_dir) : base (remote_dir)
 		{
-			Files.Add (local);
-			
+			if (local == null)
+				throw new ArgumentNullException ("local");
+			if (!File.Exists (local))
+				throw new ArgumentException ("local", "The file does not exist.");
+			Files.Add (local);			
 		}
 
-		public string FileName {
+		/// <summary>
+		/// The path of the file to put on the server.
+		/// </summary>
+		public string FilePath {
 			get { return Files [0]; }
 		}
 	}
